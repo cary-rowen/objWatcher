7e89f5ef4d9be3cb8b9a7516350b2bbad6088ad9
hwf1324 <1398969445@qq.com>
2025-03-23 18:25:30 +0800

    Add EditableListBox control

--- addon/globalPlugins/objWatcher/settings.py
+++ addon/globalPlugins/objWatcher/settings.py
@@ -9,6 +9,10 @@ import gui
 from gui import guiHelper
 from gui import nvdaControls
 
+import wx
+import wx.adv
+
+
 addonHandler.initTranslation()
 
 
@@ -28,5 +32,27 @@ class ObjWatcherPanel(gui.settingsDialogs.SettingsPanel):
 			initial=int(config.conf["objWatcher"]["interval"])
 		)
 
+		self._appendWatchAttributesList(settingsSizerHelper)
+
+	def _appendWatchAttributesList(self, settingsSizerHelper: guiHelper.BoxSizerHelper) -> None:
+		self.watchAttributesList: wx.adv.EditableListBox = wx.adv.EditableListBox(
+			self,
+			label=_("Watch attributes"),
+		)
+		self.watchAttributesList.SetStrings(config.conf["objWatcher"]["watchAttributes"].split(","))
+		editBtn: wx.BitmapButton = self.watchAttributesList.GetEditButton()
+		editBtn.SetLabel(editBtn.GetToolTipText())
+		newBtn: wx.BitmapButton = self.watchAttributesList.GetNewButton()
+		newBtn.SetLabel(newBtn.GetToolTipText())
+		delBtn: wx.BitmapButton = self.watchAttributesList.GetDelButton()
+		delBtn.SetLabel(delBtn.GetToolTipText())
+		upBtn: wx.BitmapButton = self.watchAttributesList.GetUpButton()
+		upBtn.SetLabel(upBtn.GetToolTipText())
+		downBtn: wx.BitmapButton = self.watchAttributesList.GetDownButton()
+		downBtn.SetLabel(downBtn.GetToolTipText())
+
+		settingsSizerHelper.addItem(self.watchAttributesList)
+
 	def onSave(self):
 		config.conf["objWatcher"]["interval"] = self.intervalEdit.Value
+		config.conf["objWatcher"]["watchAttributes"] = ",".join(self.watchAttributesList.GetStrings())
